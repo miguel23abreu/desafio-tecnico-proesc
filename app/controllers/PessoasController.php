0c800c7c9eaf8f0a0b6081b35f8b1ff704d7ecd9
miguel abreu <guelabreu@hotmail.com>
2025-11-23 19:42:33 -0300

    refactor - tarefa 5 sit 1: ajustamento na função cadastrarPessoa, que está funcionamento devidamente para realizar cadastro de pessoas sem nenhum erro e visualizarFormulario está enviados os grupos disponíveis

--- app/controllers/PessoasController.php
+++ app/controllers/PessoasController.php
@@ -1,13 +1,24 @@
 <?php
 
+use Illuminate\Http\Request;
+
 class PessoasController extends BaseController {
 
     public function visualizarFormulario() {
-        return View::make('formularios.cadastro');
+        $grupos = Grupo::all()->lists('nome', 'id');
+        return View::make('formularios.cadastro', compact('grupos'));
     }
 
     public function cadastrarPessoa() {
+        $pessoa = new Pessoa;
+        $pessoa->nome = Input::get('nome');
+        $pessoa->email = Input::get('email');
+        $pessoa->cpf = preg_replace('/[^0-9]/', '', Input::get('cpf'));
+        $pessoa->telefone = Input::get('telefone');
+        $pessoa->grupo_id = Input::get('grupo_id');
+        $pessoa->save();
 
         return Redirect::to('/cadastro')->with('success', 'Cadastro realizado com sucesso!');
+        
     }
 }
