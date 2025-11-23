31bcf0e303aa5df90ec23428b3df7aa30f48ffab
miguel abreu <guelabreu@hotmail.com>
2025-11-23 19:47:48 -0300

    refactor - tarefa 4: ajustes no layout do boletim, exibindo nota mínima 70 e nota máxima 100 pra cada periodo, além de deixar as notas abaixo de 70 com o fundo vermelho para destaque e fundo verde para as notas entre 70 e 100

--- app/views/relatorios/boletim.blade.php
+++ app/views/relatorios/boletim.blade.php
@@ -3,12 +3,42 @@
 @section('title', 'BOLETIM')
 
 @section('content')
-    <div class="container mt-5">
+    <style>
+        .tabela-boletim {
+            width: 80%;
+            font-size: 13px; 
+            text-align: center;
+            margin: 0 auto;
+        }
+
+        .tabela-boletim-handler {
+            width: 80%;
+            font-size: 13px; 
+            text-align: left;
+            margin: 0 auto;
+        }
+
+        .tabela-boletim th,
+        .tabela-boletim td {
+            padding: 6px; 
+            white-space: nowrap; 
+        }
+
+        .tabela-boletim thead th {
+            font-size: 14px;
+        }
+
+        .container-boletim {
+            max-width: 98%; 
+        }
+    </style>
+
+    <div class="container-boletim mt-5">
         <h2 class="text-center mb-4">BOLETIM</h2>
 
         <div class="row">
             <div class="col-md-12">
-                <table class="table table-bordered mb-4">
+                <table class="table table-bordered mb-4 tabela-boletim-handler">
                     <tbody>
                         <tr>
                             <th scope="row" class="text-left">ALUNO: {{ $aluno->nome }}</th>
@@ -22,12 +52,12 @@
         <div class="row">
             <div class="col-md-12">
                 <h4 class="text-center mb-3">NOTAS</h4>
-                <table class="table table-bordered">
+                <table class="table table-bordered tabela-boletim">
                     <thead>
                         <tr>
-                            <th scope="col">DISCIPLINAS</th>
+                            <th style="text-align: center; font-size: 14px;" scope="col">DISCIPLINAS</th>
                             @foreach ($diarios as $diario)
-                                <th scope="col"> {{ $diario->periodo_nome }} </th>
+                                <th scope="col" colspan="3"> {{ $diario->periodo_nome }} </th>
                             @endforeach
                                 <th scope="col"> {{ 'NOTA FINAL' }} </th>
                         </tr>
@@ -40,7 +70,13 @@
                                         @if ($avaliacao->disciplina_id == $disciplina->disciplina_id)
                                             @foreach ($notas_periodos as $nota_periodo)
                                                 @if ($nota_periodo->avaliacao_id == $avaliacao->avaliacao_id && $nota_periodo->disciplina_id == $avaliacao->disciplina_id)
-                                                    <td> {{ $nota_periodo->valor_nota }} </td>
+                                                    <td>N. mínima: 70</td>
+                                                    <td>N. máxima: 100</td>
+                                                    @if ($nota_periodo->valor_nota < 70)
+                                                        <td style="background-color: red; color: white;"> {{ $nota_periodo->valor_nota }} </td>
+                                                    @else
+                                                        <td style="background-color: green; color: white;"> {{ $nota_periodo->valor_nota }} </td>
+                                                    @endif
                                                 @endif
                                             @endforeach
                                         @endif
@@ -48,7 +84,11 @@
 
                                     @foreach ($notas_finais as $nota_final)
                                         @if ($nota_final['disciplina_id'] == $disciplina->disciplina_id)
-                                            <td> {{$nota_final['valor_nota']}} </td>
+                                            @if ($nota_final['valor_nota'] < 70)
+                                                <td style="background-color: red; color: white;"> {{ $nota_final['valor_nota'] }} </td>
+                                            @else
+                                                <td style="background-color: green; color: white;"> {{ $nota_final['valor_nota'] }} </td>
+                                            @endif
                                         @endif
                                     @endforeach
                             </tr>
